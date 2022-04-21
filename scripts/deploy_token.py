aec2916b44a0a46d991a09752466af7752d1dcbf
0xKalakaua <0xKalakaua@gmail.com>
2022-04-21 15:18:49 +0200

    Change deploy token to Arcane Relic from Loot

--- scripts/deploy_token.py
+++ scripts/deploy_token.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 import os
-from brownie import Loot, accounts, network, config
+from brownie import ArcaneRelic, accounts, network, config
 
 def main():
     work = accounts.load("work")
     print(network.show_active())
     publish_source = True # Not supported on Fantom Testnet
-    Loot.deploy({"from": work}, publish_source=publish_source)
+    ArcaneRelic.deploy({"from": work}, publish_source=publish_source)
